613620206eb2185df3fca996c4b278c0170510dd
Dario Kupec <dariokupec@gmail.com>
2023-03-14 10:34:57 -0400

    deploy weddings-post-tales v1

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			height: {
+				'720': '45rem',
+				'480': '30rem',
+				'448': '28rem',
+				'416': '26rem',
+			  }
+		},
+		fontFamily: {
+			'sans': ['Roboto', 'sans-serif'],
+			'montserrat': ['Montserrat', 'sans-serif'],
+			'yesteryear': ['Yesteryear', 'sans-serif'],
+			'dancing': ['Dancing Script', 'Roboto', 'sans-serif'],
+	},
+	plugins: [],
+}}
